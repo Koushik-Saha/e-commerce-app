4e5c3dad30bdc5fd9c5248b62a8f89ea9a054d9c
Koushik Saha <Koushik.saha666@gmail.com>
2019-08-21 20:24:32 +0600

    Register Design with Mainactivity onclick

---
+++ app/src/main/java/com/example/codingcafe/ecommerce/RegisterActivity.java
@@ -0,0 +1,24 @@
+package com.example.codingcafe.ecommerce;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button CreateAccountButton;
+    private EditText InputName, InputPhoneNumber, InputPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        CreateAccountButton = (Button) findViewById(R.id.register_btn);
+        InputName = (EditText) findViewById(R.id.register_username_input);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+        InputPhoneNumber = (EditText) findViewById(R.id.register_phone_number_input);
+    }
+}
