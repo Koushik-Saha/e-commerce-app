53653254c3e20880a604162fb396d492245cf084
Koushik Saha <Koushik.saha666@gmail.com>
2019-08-21 16:05:13 +0600

    Login Design Using rey5137-material and modify MainActivity and gradle changes

--- app/src/main/java/com/example/codingcafe/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/codingcafe/ecommerce/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.codingcafe.ecommerce;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button)findViewById(R.id.main_login_btn);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
