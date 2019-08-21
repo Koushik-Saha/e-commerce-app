e4015f8ae1253822eed25b828b5ef0f4d90dac9b
Koushik Saha <Koushik.saha666@gmail.com>
2019-08-21 14:21:02 +0600

    Firebase connection with android app and Main activity design with main screen and all logo add to the drawable and design color and style.and drawable button and inputs

---
+++ app/src/main/java/com/example/codingcafe/ecommerce/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.codingcafe.ecommerce;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
+        loginButton = (Button)findViewById(R.id.main_login_btn);
+    }
+}
