f53029b9a4859949f06d5dbd0c60c9c46b1d623f
Koushik Saha <Koushik.saha666@gmail.com>
2019-08-23 00:33:53 +0600

    Admin Add New Product Activity in AdminAddNewProductActivity

--- app/src/main/java/com/example/codingcafe/ecommerce/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/codingcafe/ecommerce/AdminAddNewProductActivity.java
@@ -4,11 +4,17 @@ package com.example.codingcafe.ecommerce;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
-    private String CategoryName;
+    private String CategoryName, Description, Price, Pname, saveCurrentDate, saveCurrentTime;
+    private Button AddNewProductButton;
+    private ImageView InputProductImage;
+    private EditText InputProductName, InputProductDescription, InputProductPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,11 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         CategoryName = getIntent().getExtras().get("category").toString();
 
 
-        Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show();
+        AddNewProductButton = (Button) findViewById(R.id.add_new_product);
+        InputProductImage = (ImageView) findViewById(R.id.select_product_image);
+        InputProductName = (EditText) findViewById(R.id.product_name);
+        InputProductDescription = (EditText) findViewById(R.id.product_description);
+        InputProductPrice = (EditText) findViewById(R.id.product_price);
 
 
 
