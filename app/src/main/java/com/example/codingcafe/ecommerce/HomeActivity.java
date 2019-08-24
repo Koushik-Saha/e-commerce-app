987558d9da2aa186a62eb2e1d3e2a9ddda463abb
Koushik Saha <Koushik.saha666@gmail.com>
2019-08-24 21:46:48 +0600

    allow user to upload image to firebase storage and firebase database

--- app/src/main/java/com/example/codingcafe/ecommerce/HomeActivity.java
+++ app/src/main/java/com/example/codingcafe/ecommerce/HomeActivity.java
@@ -86,7 +86,7 @@ public class HomeActivity extends AppCompatActivity
         CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
 
         userNameTextView.setText(Prevalent.currentOnlineUser.getName());
-//        Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
+        Picasso.get().load(Prevalent.currentOnlineUser.getImage()).placeholder(R.drawable.profile).into(profileImageView);
 
 
         recyclerView = findViewById(R.id.recycler_menu);
